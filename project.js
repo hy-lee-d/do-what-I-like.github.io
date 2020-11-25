2843356269b21d1e459ed3d2e4e2ba1f23425028
hy-lee-d <74891883+hy-lee-d@users.noreply.github.com>
2020-11-25 17:52:31 +0900

    Create project.js

---
+++ project.js
@@ -0,0 +1,14 @@
+<script>
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "kik09hocvno9d2gb-ufksxhtayfiesnki-nqxdskqgv3jppsf6"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
+</script>
